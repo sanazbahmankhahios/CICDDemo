53aa197b2fe1bb24d9b443eff595bce0af8ec3c4
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-09-16 11:03:50 +0100

    Initial commit

---
+++ CICD/CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDApp.swift
+//  CICD
+//
+//  Created by sanaz on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
